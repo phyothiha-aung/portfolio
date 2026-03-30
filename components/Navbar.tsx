18d948328e67278c2208be7e0d89b7c038601478
Phyo Thiha Aung <phyothihaaung@myansis.com>
2026-03-30 16:42:31 +0630

    fix mobile navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,13 +8,16 @@ const links = ["Home", "About", "Experience", "Projects", "Skills", "Contact"];
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const menuRef = useRef<HTMLDivElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
+      const target = event.target as Node;
       if (
         isOpen &&
         menuRef.current &&
-        !menuRef.current.contains(event.target as Node)
+        !menuRef.current.contains(target) &&
+        !buttonRef.current?.contains(target)
       ) {
         setIsOpen(false);
       }
@@ -57,6 +60,7 @@ const Navbar = () => {
         </ul>
 
         <button
+          ref={buttonRef}
           onClick={() => setIsOpen(!isOpen)}
           className="md:hidden text-2xl cursor-pointer text-primary"
         >
@@ -66,7 +70,7 @@ const Navbar = () => {
         {isOpen && (
           <div
             ref={menuRef}
-            className="md:hidden absolute mt-3 bg-background w-[90%] mx-auto top-20 border border-white/10 rounded-2xl px-6 py-6 text-white shadow-2xl"
+            className="md:hidden absolute bg-background w-[90%] mx-auto top-19 border border-white/10 rounded-2xl px-6 py-6 text-white shadow-2xl"
           >
             <div className="flex flex-col gap-5 text-center">
               {links.map((item) => (
